1e9fa1cdca5d2af8e54d9ff76a9029374c56bb14
Dev-Aryan-coder <aryan.sharma@vsit.edu.in>
2026-08-30 12:00:18 +0530

    fix: add selfRating getter and setter to AiAssessmentRequestDTO

--- src/main/java/com/example/TalentOrbit/dto/request/AiAssessmentRequestDTO.java
+++ src/main/java/com/example/TalentOrbit/dto/request/AiAssessmentRequestDTO.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class AiAssessmentRequestDTO {
     private Long userId;
     private Long skillId;
-    private Integer selfRatingOutOf10; // e.g. 8 for 8/10
+    private Integer selfRatingOutOf10;
+    private Integer selfRating;
     private List<AssessmentQuestionAnswerDTO> answers;
 
     public AiAssessmentRequestDTO() {}
@@ -16,8 +17,21 @@ public class AiAssessmentRequestDTO {
     public Long getSkillId() { return skillId; }
     public void setSkillId(Long skillId) { this.skillId = skillId; }
 
-    public Integer getSelfRatingOutOf10() { return selfRatingOutOf10; }
-    public void setSelfRatingOutOf10(Integer selfRatingOutOf10) { this.selfRatingOutOf10 = selfRatingOutOf10; }
+    public Integer getSelfRatingOutOf10() { 
+        return selfRatingOutOf10 != null ? selfRatingOutOf10 : selfRating; 
+    }
+    public void setSelfRatingOutOf10(Integer selfRatingOutOf10) { 
+        this.selfRatingOutOf10 = selfRatingOutOf10;
+        this.selfRating = selfRatingOutOf10;
+    }
+
+    public Integer getSelfRating() { 
+        return selfRating != null ? selfRating : selfRatingOutOf10; 
+    }
+    public void setSelfRating(Integer selfRating) { 
+        this.selfRating = selfRating; 
+        this.selfRatingOutOf10 = selfRating;
+    }
 
     public List<AssessmentQuestionAnswerDTO> getAnswers() { return answers; }
     public void setAnswers(List<AssessmentQuestionAnswerDTO> answers) { this.answers = answers; }
